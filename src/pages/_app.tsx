a5b734670afe60ea5a5e229eb9660cf58bcae2f8
Renan Pupin <renan.pupin@gmail.com>
2023-03-14 12:38:21 -0300

    initial commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,13 @@
+import '@/styles/globals.css'
+import type { AppProps } from 'next/app'
+
+import { ApolloProvider } from '@apollo/client'
+import { useApollo } from '../apollo/client'
+
+
+export default function App({ Component, pageProps }: AppProps) {
+  const apolloClient = useApollo(pageProps.initialApolloState)
+  return <ApolloProvider client={apolloClient}>
+    <Component {...pageProps} />
+  </ApolloProvider>
+}
